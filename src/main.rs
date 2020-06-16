423536df50d5b2dcbcf38f543cb5fcd405facb9a
Christian Wood <cwood821@gmail.com>
2020-06-16 09:49:33 -0500

    Filter to only 200 results

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use reqwest::Client;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::io::{BufReader};
-use std::path::Path;
+use reqwest::Response;
 use futures::future::join_all;
 use structopt::StructOpt;
 mod conf;
@@ -16,13 +13,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let patterns = patterns::get_patterns(target_url);
 
   let client = Client::builder().build()?;
-  let values = futures::future::join_all(
+  let results = join_all(
     patterns.iter()
     .map(|url| {
         client.get(url).send()
     })
   ).await;
 
-  println!("{:#?}", values);
+  let request_count = results.len(); 
+  let warnings: Vec<Response> = results.into_iter().filter_map(|res| res.ok() ).filter(|res| res.status() == 200 ).collect();
+
+  println!("Results:");
+  println!("Requests: {}", request_count);
+  println!("{:#?}", warnings);
   Ok(())
 }
--- src/patterns.rs
+++ src/patterns.rs
@@ -4,19 +4,12 @@ use std::io::{BufReader};
 use std::path::Path;
 
 pub fn get_patterns(url: String) -> Vec<String> {
-  let filename = "src/main.rs";
-  let file = File::open(filename).unwrap();
-  let reader = BufReader::new(file);
-  
   let mut patterns = Vec::new();
 
   if let Ok(lines) = read_lines("./src/data/config_patterns") {
-    for line in lines {
-      if let Ok(pattern) = line {
-        let route = format!("{}/{}", url, pattern);
-        patterns.push(route);
-      }
-    }
+    patterns = lines.filter_map(|line| line.ok())
+      .map(|line| format!("{}/{}", url, line))
+      .collect()
   }
 
   patterns
