63ae334d2de64e2ac55c1cfd094b62a1299ebdd9
Christian Wood <cwood821@gmail.com>
2020-06-17 17:02:25 -0500

    Read config from yaml

--- src/http_tests.rs
+++ src/http_tests.rs
@@ -1,48 +1,42 @@
 use std::fs;
-use std::fs::File;
 use std::io::{self, BufRead, Error};
-use std::io::{BufReader};
-use std::path::Path;
 extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+use yaml_rust::yaml;
+use yaml_rust::{YamlLoader};
 
+#[derive(Debug)]
 pub struct HTTPTest {
-  host: String,
-  path: String,
-  status: i32 // this could be smaller
+  pub host: String,
+  pub path: String,
+  pub status: i64 // this could be smaller
 } 
 
 pub fn parse_tests(config_path: String) -> Result<Vec<HTTPTest>, Error>  {
-  let mut tests = Vec::new();
+  let mut mapped_tests: Vec<HTTPTest> = Vec::new();
 
   let yaml = fs::read_to_string(config_path)?;
   let parsed = YamlLoader::load_from_str(&yaml).unwrap();
   
   let config = &parsed[0];
 
-  let hosts = config["hosts"].as_vec();
-  println!("{:#?}", hosts);
-  // map structure to HTTPTest structs
+  // This is ugly, log failures to parse and clean up to a map structure
+  if let Some(hosts) = config["hosts"].to_owned().into_vec() {
+    for host in hosts {
+      let cur_host = host["host"].as_str().unwrap();
+      if let Some(tests) = host["tests"].to_owned().into_vec() {
+        for test in tests {
+          let new_test = HTTPTest {
+            host: cur_host.to_string(),
+            path: format!("{}{}", cur_host, test["path"].as_str().unwrap().to_string()),
+            status: test["status"].as_i64().unwrap()
+          };
+  
+          mapped_tests.push(new_test); 
+        }
+      }
+    }
+  }
+  // println!("{:#?}", mapped_tests);
 
-  Ok(tests)
+  Ok(mapped_tests)
 }
-// Map 
-
-// pub fn get_patterns(url: String) -> Vec<String> {
-//   let mut patterns = Vec::new();
-
-//   if let Ok(lines) = read_lines("./src/data/config_patterns") {
-//     patterns = lines.filter_map(|line| line.ok())
-//       .map(|line| format!("{}/{}", url, line))
-//       .collect()
-//   }
-
-//   patterns
-// }
-
-// // The output is wrapped in a Result to allow matching on errors
-// // Returns an Iterator to the Reader of the lines of the file.
-// fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path> {
-//     let file = File::open(filename)?;
-//     Ok(io::BufReader::new(file).lines())
-// }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use futures::future::join_all;
 use structopt::StructOpt;
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader, YamlEmitter};
+use std::io::ErrorKind;
 mod conf;
 mod http_tests;
 
@@ -12,29 +13,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let opt = conf::Opt::from_args();
   let config = opt.config;
 
-  // let patterns = patterns::get_patterns(target_url);
-  let patterns: Vec<String> = Vec::new();
+  if let Ok(tests) = http_tests::parse_tests(config) {
+    let client = Client::builder().build()?;
 
+    let results = join_all(
+      tests.iter()
+        .map(|test| {
+            let url = format!("{}{}", test.host, test.path);
+            client.get(&url).send()
+        })
+    ).await;
 
-  let client = Client::builder().build()?;
-  let results = join_all(
-    patterns.iter()
-    .map(|url| {
-        client.get(url).send()
-    })
-  ).await;
+    let request_count = results.len(); 
 
-  let request_count = results.len(); 
-  let warnings: Vec<Response> = results.into_iter()
-    .filter_map(|res| res.ok())
-    .filter(|res| res.status() == 200)
-    .collect();
+    println!("Requests: {}", request_count);
+    println!("Results {:#?}", results);
+    Ok(())
+  } else {
+    eprintln!("Failed to parse configuration");
+    Ok(())
+  }
 
-  http_tests::parse_tests(config);
-  
-
-  println!("Results:");
-  println!("Requests: {}", request_count);
-  println!("{:#?}", warnings);
-  Ok(())
 }
